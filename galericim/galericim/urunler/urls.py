997d2a4f3ea43b57e19f925d1558ef307102becb
Muhammed Emin ÇAĞLAR <em1nc4glar@gmail.com>
2023-05-21 23:39:49 +0300

    added login and register

--- galericim/galericim/urunler/urls.py
+++ galericim/galericim/urunler/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 # http://127.0.0.1:8000/
-# http://127.0.0.1:8000/home
-# http://127.0.0.1:8000/advert
-# http://127.0.0.1:8000/login
-# http://127.0.0.1:8000/register
+
 
 
 
--- galericim/galericim/uyelikler/views.py
+++ galericim/galericim/uyelikler/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User
 
 def login_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username = username, password= password)
+        if user is not None:
+            login(request, user)
+            return redirect("user_home")
+        else:
+            return render(request, "uyelikler/login.html",{
+                "error": "Kullanıcı adı ya da Şifre hatalı"
+            })
     return render(request,"uyelikler/login.html")
 
 
@@ -9,10 +23,31 @@ def advert_request(request):
 
 
 def register_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username = username).exists():
+                return render(request,"register", {"error":"Bu Kullanıcı Adı Kullanılıyor."})
+            else:
+                if User.objects.filter(email = email).exists():
+                    return render(request,"register", {"error":"Bu E-Mail Kullanılıyor."})
+                else:
+                    user = User.objects.create_user(username=username,email=email,first_name=firstname,last_name=lastname,password=password)
+                    user.save()
+                    return redirect("login")
+        else:
+            return render(request,"register", {"error":"Parola eşleşmiyor."})
     return render(request,"uyelikler/register.html")
 
 
 def logout_request(request):
+    logout(request)
     return redirect("home")
 
 def home_request(request):
